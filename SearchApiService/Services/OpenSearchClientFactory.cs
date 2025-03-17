1655a1c2616768ce73592c69f05872d86f2ce264
Naman Agrawal <naman.agrawal@nagarro.com>
2025-03-17 18:46:15 +0530

    add opensearch env

--- SearchApiService/Services/OpenSearchClientFactory.cs
+++ SearchApiService/Services/OpenSearchClientFactory.cs
@@ -8,10 +8,14 @@ namespace SearchApiService.Services
         public static IOpenSearchClient openSearchClient(IConfiguration config)
         {
             var (uri, user, pw, defaultIdx) = (
-                config.GetSection("OpenSearch:Uri").Value,
-                config.GetSection("OpenSearch:Username").Value,
-                config.GetSection("OpenSearch:Password").Value,
-                config.GetSection("OpenSearch:DefaultIndex").Value
+                Environment.GetEnvironmentVariable("OPENSEARCH_URI") 
+                    ?? config.GetSection("OpenSearch:Uri").Value,
+                Environment.GetEnvironmentVariable("OPENSEARCH_USERNAME")
+                    ?? config.GetSection("OpenSearch:Username").Value,
+                Environment.GetEnvironmentVariable("OPENSEARCH_PASSWORD")
+                    ?? config.GetSection("OpenSearch:Password").Value,
+                Environment.GetEnvironmentVariable("OPENSEARCH_DefaultIndex")
+                    ?? config.GetSection("OpenSearch:DefaultIndex").Value
             );
 
             var settings = new ConnectionSettings(new SingleNodeConnectionPool(new Uri(uri ?? "")))
